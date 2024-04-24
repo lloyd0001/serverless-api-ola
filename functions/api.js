8b950298eb45aa07ac90ccc40697b173472e90ed
Lloyd <JohnLloyd15102002@gmail.com>
2024-04-25 00:29:55 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const router = require('./routes/author');
-const mongoose = require('mongoose');
-const cors = require('cors');
+import express, { json, urlencoded } from 'express';
+import serverless from 'serverless-http';
+import router from './routes/author';
+import { connect } from 'mongoose';
+import cors from 'cors';
 
 const app = express();
 
@@ -10,13 +10,12 @@ const dbCloudUrl =
 'mongodb+srv://johnlloyd15:QyOwQvnki75ZaAvP@cluster0.0ycwtxv.mongodb.net/?retryWrites=true&w=majority';
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
-mongoose
-.connect(dbCloudUrl || dbLocalUrl)
+connect(dbCloudUrl || dbLocalUrl)
 .then(() => console.log('Connected to MongoDB'))
 .catch((error) => console.error('Failed to connect to MongoDB', error));
 
 app.use('/.netlify/functions/api', router);
-module.exports.handler = serverless(app);
+export const handler = serverless(app);
