84f40207fe8a511b79c8433e2e15e623b6cf6cd0
Lloyd <JohnLloyd15102002@gmail.com>
2024-04-25 00:39:37 +0800

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -13,9 +13,13 @@ app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-connect(dbCloudUrl || dbLocalUrl)
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.error('Failed to connect to MongoDB', error));
-
-app.use('/.netlify/functions/api', router);
-export const handler = serverless(app);
+mongoose
+  .connect(dbCloudUrl)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch(err => {
+    console.log('Error connecting to MongoDB', err)
+  })
+app.use('/.netlify/functions/api', router)
+module.exports.handler = serverless(app)
